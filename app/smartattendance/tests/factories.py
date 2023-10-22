18ea80d36d5aebde0a733c7a94c2a80e851fc0e7
Mauricio <mauriciolfsilva@gmail.com>
2023-10-21 22:34:48 -0300

    Testes dos models finalizados

--- app/smartattendance/tests/factories.py
+++ app/smartattendance/tests/factories.py
@@ -6,6 +6,7 @@ from random import randint
 from .CustomProviders import *
 from ..models import *
 from ..lib import *
+from datetime import datetime, timedelta
 
 #Arquivo de factories para testes unitários
 #Autor: Mauricio
@@ -23,50 +24,35 @@ class UsuarioFactory(django.DjangoModelFactory):
     class Meta:
         model = Usuario
 class TurmaFactory(django.DjangoModelFactory):
-    def __init__(self, prof = -1):
-        self.professor_id = prof if prof > 0 else randint(10,50)
-        self.semestre = fake.semestre()
-        self.turma_nome = fake.turma_nome()
+    semestre = fake.semestre()
+    turma_nome = fake.turma_nome()
 
     class Meta:
         model = Turma
 
 class Aluno_TurmaFactory(django.DjangoModelFactory):
 
-    def __init__(self, aluno = -1, turma = -1):
-        self.aluno_id = aluno if aluno > 0 else randint(10,50)
-        self.turma_id = turma if turma > 0 else randint(10,50)
-
     class Meta:
         model = Aluno_Turma
 
 class ChamadaFactory(django.DjangoModelFactory):
-
-    def __init__(self, inicio, fim, turma = -1):
-        self.turma_id = turma if turma > 0 else randint(10,50)
-        self.data_inicio = inicio
-        self.data_fim = fim
-        self.latitude = 10.0
-        self.longitude = 15.2
+    data_inicio = datetime.now().strftime('%Y-%m-%d-%H')
+    data_fim = (datetime.now() + timedelta(hours=2)).strftime('%Y-%m-%d-%H')
+    latitude = 10.0
+    longitude = 15.2
     
     class Meta:
         model = Chamada
 
 class PresencaFactory(django.DjangoModelFactory):
-    def __init__(self, inicio, fim, aluno = -1, chamada = -1):
-        self.aluno_id = aluno if aluno > 0 else randint(10,50)
-        self.chamada_id = chamada if chamada > 0 else randint(10,50)
-        self.tempo_entrada = inicio
-        self.tempo_saida = fim
+    tempo_entrada = datetime.now().strftime('%Y-%m-%d-%H')
+    tempo_saida = (datetime.now() + timedelta(hours=2)).strftime('%Y-%m-%d-%H')
     class Meta:
         model = Presenca
 
 class Turma_HorarioFactory(django.DjangoModelFactory):
-
-    def __init__(self, inicio, fim, turma = -1, diasemana = 0):
-        self.turma_id = turma if turma > 0 else randint(10,50)
-        self.dia_semana = WeekdayMap[diasemana][0]
-        self.hora_inicio = inicio
-        self.hora_fim = fim
+    dia_semana = WeekdayMap[0][0]
+    hora_inicio = datetime.now().strftime('%H')
+    hora_fim = (datetime.now() + timedelta(hours=2)).strftime('%H')
     class Meta:
         model = Turma_Horario
--- app/smartattendance/tests/test_models.py
+++ app/smartattendance/tests/test_models.py
@@ -1,17 +1,57 @@
 from django.test import TestCase
 
 from .factories import *
+from ..lib import WeekdayMap
 
 
 class UsuarioTestCase(TestCase):
     def test_str(self):
-        """Test for string representation."""
-        usuario = UsuarioFactory()
-        self.assertEqual('A', usuario.usuario_tipo)
+        """Teste criando usuário e associando a um tipo específico"""
+        usuario = UsuarioFactory(usuario_tipo='P')
+        self.assertEqual('P', usuario.usuario_tipo)
 
 class TurmaTestCase(TestCase):
     def test_str(self):
-        """Test for string representation."""
-        usuario = UsuarioFactory()
-        turma = TurmaFactory()
-        self.assertEqual(1, turma.professor_id)
+        """Teste criando uma turma com um professor"""
+        usuario = UsuarioFactory(usuario_tipo='P')
+        turma = TurmaFactory(professor_id=usuario)
+        self.assertEqual(usuario, turma.professor_id)
+
+class Aluno_TurmaTestCase(TestCase):
+    def test_str(self):
+        """Teste, associando um aluno a uma turma"""
+        professor = UsuarioFactory(usuario_tipo='P')
+        aluno = UsuarioFactory()
+        turma = TurmaFactory(professor_id=professor)
+        aluno_turma = Aluno_TurmaFactory(aluno_id=aluno, turma_id=turma)
+        self.assertEqual(aluno, aluno_turma.aluno_id)
+        self.assertEqual(turma, aluno_turma.turma_id)
+
+class Turma_HorarioTestCase(TestCase):
+    def test_str(self):
+        """Teste, associando Turma_Horario a uma turma"""
+        professor = UsuarioFactory(usuario_tipo='P')
+        turma = TurmaFactory(professor_id=professor)
+        turma_horario = Turma_HorarioFactory(turma_id=turma, dia_semana=WeekdayMap[2][0])
+        self.assertEqual(turma, turma_horario.turma_id)
+        self.assertEqual("Qua", turma_horario.dia_semana)
+
+class ChamadaTestCase(TestCase):
+    def test_str(self):
+        """Teste, associando uma Chamada à turma"""        
+        professor = UsuarioFactory(usuario_tipo='P')
+        turma = TurmaFactory(professor_id=professor)
+        data = datetime.now().strftime('%Y-%m-%d-%H')
+        chamada = ChamadaFactory(turma_id=turma, data_inicio=data)
+        self.assertEqual(turma, chamada.turma_id)
+        self.assertEqual(data, chamada.data_inicio)
+
+class PresencaTestCase(TestCase):
+    def test_str(self):
+        """Teste, associando uma presença a uma chamada"""
+        professor = UsuarioFactory(usuario_tipo='P')
+        turma = TurmaFactory(professor_id=professor)
+        data = datetime.now().strftime('%Y-%m-%d-%H')
+        chamada = ChamadaFactory(turma_id=turma, data_inicio=data)
+        aluno = UsuarioFactory()
+        
