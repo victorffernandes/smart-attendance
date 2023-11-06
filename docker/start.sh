d3c63e3f28efd59d83f070451747e0736838c2cb
Victor Faria Fernandes <victorfariafernandes@gmail.com>
2023-11-05 21:21:38 -0300

    update

--- docker/start.sh
+++ docker/start.sh
@@ -1,4 +1,4 @@
 sleep 20
 python app/manage.py migrate
 python app/manage.py loaddata app/smartattendance/fixtures/initial_data.json
-python app/manage.py runserver 0.0.0.0:80
+python app/manage.py runserver 0.0.0.0:8000
