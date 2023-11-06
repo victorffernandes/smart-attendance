92c2e261504aecf641f4315c148a29dcd8ac7948
Victor Faria Fernandes <victorfariafernandes@gmail.com>
2023-11-05 23:07:00 -0300

    update

--- app/smartattendance/urls.py
+++ app/smartattendance/urls.py
@@ -38,8 +38,7 @@ schema_view = get_schema_view(
       license=openapi.License(name="BSD License"),
    ),
    public=True,
-   permission_classes=(permissions.AllowAny,),
-   url='https://smartattendances.online/'
+   permission_classes=(permissions.AllowAny,)
 )
 
 urlpatterns = [
