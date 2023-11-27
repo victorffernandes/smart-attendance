85c6323775809efe73ad783b6e1482fe500f9211
Mauricio <mauriciolfsilva@gmail.com>
2023-11-27 20:22:16 -0300

    Rota de inserção e retorno finalizadas

--- app/smartattendance/serializers/AtestadoSerializer.py
+++
@@ -1,9 +0,0 @@
-
-from rest_framework.serializers import Serializer, FileField, IntegerField
-
-# Serializers define the API representation.
-class UploadSerializer(Serializer):
-    arquivo = FileField()
-    presenca_id = IntegerField()
-    class Meta:
-        fields = ['presenca_id','arquivo']
--- app/smartattendance/views/Atestado.py
+++ app/smartattendance/views/Atestado.py
@@ -1,17 +1,49 @@
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.parsers import MultiPartParser
+from django.core.files import File
+from django.http import HttpResponse
+from datetime import datetime
 
 from ..models import Presenca
-from ..serializers import AtestadoSerializer
+from ..serializers import PresencaSerializer
 
 class ViewSet(GenericViewSet):
 
-    serializer_class = AtestadoSerializer
+    parser_classes = [MultiPartParser]
+    queryset = Presenca.objects.all()
+    serializer_class = PresencaSerializer.Serializer
 
     @action(detail=False, methods=['POST'])
-    def inserir(self, request):
-        arquivo = request.FILES.get('arquivo')
-        content_type = arquivo.content_type
-        response = "POST API and you have uploaded a {} file".format(content_type)
-        return Response(response)
+    def inserir(self, request, format=None):
+        file_obj = request.data['atestado']
+        chamada = request.data['chamada']
+        aluno = request.data['aluno']
+
+        presenca = Presenca.objects.get(chamada=chamada, aluno=aluno)
+        path = 'var/lib/atestado/' + str(presenca.id) + '.jpg'
+        presenca.status = 'C'
+        presenca.caminho_atestado = path
+        presenca.ultima_atualizacao = datetime.now()
+
+        
+        destination = open(path, 'wb+')
+        for chunk in file_obj.chunks():
+            destination.write(chunk)
+        destination.close()
+
+        presenca.save()
+
+
+
+        return Response(status=204)
+
+    @action(detail=True, methods=['GET'])
+    def retornar(self, request, format=None, pk=None):
+        presenca = self.get_object()
+        atestado = open(presenca.caminho_atestado, 'rb')
+        filename = str(presenca.id) + '.jpg'
+        response = HttpResponse(File(atestado), content_type='application/jpg')
+        response['Content-Disposition'] = 'attachment; filename="%s"' % filename
+        return response
